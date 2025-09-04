49bfa86e75b2af32ac0286756dd1e5f2d07cc654
IA. MechMind <ia.mechmind@gmail.com>
2025-09-04 07:30:38 +0200

    Update __init__.py

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,1 +1,5 @@
-#__init__.py
+# app/database/repositories/__init__.py
+
+# Este archivo marca el directorio 'repositories' como un paquete de Python.
+# Los repositorios contendrán la lógica para interactuar con la base de datos,
+# abstrayéndola de los servicios de negocio.
