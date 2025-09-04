cd90088e0f528ae68ef9d101d03e9e3768a07db9
IA. MechMind <ia.mechmind@gmail.com>
2025-09-04 08:20:30 +0200

    Create __init__.py

---
+++ tests/test_utils/__init__.py
@@ -0,0 +1,3 @@
+# Este archivo convierte el directorio 'test_utils' en un paquete de Python.
+# Aquí se alojarán las pruebas para las funciones de utilidad,
+# como las estadísticas y las visualizaciones.
