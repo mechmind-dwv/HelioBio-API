577661136f299c78b582375723b2a61e0d637a6b
IA. MechMind <ia.mechmind@gmail.com>
2025-09-04 06:25:40 +0200

    Create __init__.py

---
+++ app/config/__init__.py
@@ -0,0 +1,5 @@
+# config/__init__.py
+
+# Este archivo hace que el directorio 'config' sea reconocible como
+# un paquete de Python. Es el punto de partida para nuestra
+# configuración de aplicación centralizada.
