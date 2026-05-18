3694dd1f7f14fbbe87e9f0039e5399a618da4b7f
root <root@localhost.localdomain>
2026-05-18 10:24:49 +0000

    Arreglar import Field en solar.py

--- app/models/solar.py
+++ app/models/solar.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, Field, field_validator
 
 
 class SolarCyclePhase(str, Enum):
